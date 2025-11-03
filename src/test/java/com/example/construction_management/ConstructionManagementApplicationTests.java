a64272fc8ff5dc5c8ed9ee8672680cbebb037ad2
NguyenAnhTuM10 <151209084+NguyenAnhTuM10@users.noreply.github.com>
2025-11-03 17:41:54 +0700

    Innitial commit

---
+++ src/test/java/com/example/construction_management/ConstructionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.construction_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
